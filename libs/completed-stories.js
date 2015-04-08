1e9f8429fb76fe9d9e8a66849c374d5653548cb2
cdonahue <clayton.donahue@gmail.com>
2015-04-08 19:57:51 -0400

    missed a file

--- libs/completed-stories.js
+++ libs/completed-stories.js
@@ -1,4 +1,4 @@
-module.exports = function(program, release, cb) {
+module.exports = function(program, releaseData, cb) {
   'use strict';
 
   var async = require('async'),
@@ -6,33 +6,79 @@ module.exports = function(program, release, cb) {
       Trello = require('node-trello');
 
   var trello = new Trello(program.apiKey, program.token),
-      trelloUtils = require('../libs/trello-utils.js');
+      trelloUtils = require('../libs/trello-utils.js'),
+      _after = require('lodash.after');
 
-  var boardId = program.boardId,
-      doneList = program.doneList.toLowerCase();
+  var doneList = program.doneList.toLowerCase();
 
-  /**
-   * Returns a list of "done" stories in the board.
-   * @param  {[type]}
-   * @param  {Function}
-   * @return {[type]}
-   */
-  var getStoriesForBoard = function(listId, cb) {
-    trello.get(format('/1/boards/%s/cards', boardId), function(err, data) {
-      if(err) {return cb(err);}
-      var doneStories = data.filter(function(item) {
-        return item.idList.toLowerCase() === listId;
+  var getBoardsFromCard = function(cb) {
+    var card = releaseData.releaseCard;
+
+    if(card.idChecklists.length > 0) {
+
+      var boardIds = [];
+      var done = _after(card.idChecklists.length, function(){
+        cb(boardIds);
+      } || function() {});
+
+      card.idChecklists.forEach(function(checklistId) {
+        async.series([function(cb) {
+          trello.get(format('/1/checklists/%s', checklistId), function(err, data) {
+            if(data.name === 'Sprints') {
+              boardIds = data.checkItems.map(function(item){
+                return item.name
+                            .replace(/.*\/b\//, '')
+                            .replace(/\/.*$/, '');
+              });
+
+            }
+            cb(null);
+          });
+        }], function(err) {
+          if(err) {return cb(err);}
+          done();
+        });
       });
-      return cb(null, doneStories);
-    });
+    } else {
+      return cb(new Error('No release boards found!'));
+    }
+
   };
 
-  async.waterfall([
-    trelloUtils.getBoardListIdByName(trello, boardId, doneList),
-    getStoriesForBoard
-  ], function(err, result) {
-    release.stories = result;
-    return err ? cb(err) : cb(null, result);
+  var getStoriesForBoard = function(boardId) {
+    return function(listId, cb) {
+      trello.get(format('/1/boards/%s/cards', boardId), function(err, data) {
+        if(err) {return cb(err);}
+        var doneStories = data.filter(function(item) {
+          return item.idList.toLowerCase() === listId;
+        });
+        return cb(null, doneStories);
+      });
+    };
+  };
+
+
+  var boardIds = getBoardsFromCard(function(boardIds) {
+
+    var done = _after(boardIds.length, function() {
+      cb(null);
+    });
+
+    boardIds.forEach(function(boardId){
+      async.waterfall([
+        trelloUtils.getBoardListIdByName(trello, boardId, doneList),
+        getStoriesForBoard(boardId)
+      ], function(err, result){
+        if(err) { return cb(err); }
+        result.forEach(function(story){
+          if(!releaseData.stories) {
+            releaseData.stories = [];
+          }
+          releaseData.stories.push(story);
+        })
+        return done();
+      });
+    });
   });
 
 };
